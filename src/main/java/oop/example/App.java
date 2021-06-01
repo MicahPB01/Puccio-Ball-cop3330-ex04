ea14da15c6dddbfe9a9215f0c4c234099c5f8f43
MicahPB01 <micahpuccioball@gmail.com>
2021-05-31 22:04:38 -0400

    Updated

--- src/main/java/oop/example/App.java
+++ src/main/java/oop/example/App.java
@@ -5,22 +5,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        Scanner getNoun = new Scanner(System.in);
-        Scanner getVerb = new Scanner(System.in);
-        Scanner getAdjective = new Scanner(System.in);
-        Scanner getAdverb = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+
 
         System.out.print("Enter a noun: ");
-        String noun = getNoun.nextLine();
+        String noun = input.nextLine();
 
         System.out.print("Enter a verb: ");
-        String verb = getVerb.nextLine();
+        String verb = input.nextLine();
 
         System.out.print("Enter an adjective: ");
-        String adjective = getAdjective.nextLine();
+        String adjective = input.nextLine();
 
         System.out.print("Enter and adverb: ");
-        String adverb = getAdverb.nextLine();
+        String adverb = input.nextLine();
 
         System.out.printf("Do you %s your %s %s %s? That's hilarious!", verb, adjective, noun, adverb);
 
